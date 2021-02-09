8b4d1a5562964a790932684955e8f0ff66cd7275
cgarcia369@misena.edu.co <cgarcia369@misena.edu.co>
2021-02-09 15:20:35 -0500

    fix: Se cambia titulo superior en la pagina editar usuario

--- 2.Analisis/Prototipos/QrleanCod/pages/dashboard/admin/personas/editar/[id].js
+++ 2.Analisis/Prototipos/QrleanCod/pages/dashboard/admin/personas/editar/[id].js
@@ -121,7 +121,7 @@ const EditarPersona = () => {
                     <svg  viewBox="0 0 20 20" fill="black" className="cursor-pointer   top-0 left-0 absolute  w-10 h-10 fill-current text-gray-800 m-4" onClick={()=>router.push('/dashboard/admin/personas')}>
                         <path d="M11.739,13.962c-0.087,0.086-0.199,0.131-0.312,0.131c-0.112,0-0.226-0.045-0.312-0.131l-3.738-3.736c-0.173-0.173-0.173-0.454,0-0.626l3.559-3.562c0.173-0.175,0.454-0.173,0.626,0c0.173,0.172,0.173,0.451,0,0.624l-3.248,3.25l3.425,3.426C11.911,13.511,11.911,13.789,11.739,13.962 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.148,3.374,7.521,7.521,7.521C14.147,17.521,17.521,14.148,17.521,10"></path>
                     </svg>
-                    <h1 className="text-3xl text-gray-800 text-center w-full my-4 ">Editar persona</h1>
+                    <h1 className="text-3xl text-gray-800 text-center w-full my-4 ">Editar registro</h1>
                     <label className="text-gray-800 text-xl mx-auto mt-4 w-11/12" htmlFor="nombre" >Nombre</label>
                     <AnimatePresence>
                         {formik.errors.nombre && formik.touched.nombre ? <motion.p  initial="initial" animate="animate" exit="unshow" variants={errorVariants} className="rounded-lg overflow-hidden text-center  text-red-600 text-base">{formik.errors.nombre}</motion.p>:null}
