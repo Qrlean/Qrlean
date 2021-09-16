e36e6806409dc3e6e7327b7624371bab450c4f79
Camilo Garcia <cgarcia369@misena.edu.co>
2021-09-16 00:04:03 -0500

    chore: se configura typeorm, swagger y container para decorators

--- CodigoProyecto/Backend/src/app.module.ts
+++ CodigoProyecto/Backend/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { FichasModule } from './fichas/fichas.module';
+import SeedDatabaseService from './database/seed/seeder';
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'React2020*',
+            database: 'Qrlean',
+            entities: ['dist/**/*.entity{.ts,.js}'],
+            synchronize: false,
+            retryDelay: 3000,
+            retryAttempts: 10,
+        }),
+        UsuariosModule,
+        AuthModule,
+        FichasModule,
+    ],
     controllers: [],
-    providers: [],
+    providers: [SeedDatabaseService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private readonly seeder: SeedDatabaseService) {
+        this.seeder.run();
+    }
+}
--- CodigoProyecto/Backend/src/main.ts
+++ CodigoProyecto/Backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+    const config = new DocumentBuilder()
+        .setTitle('Cats example')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .addTag('cats')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+    useContainer(app.select(AppModule), { fallbackOnErrors: true });
     await app.listen(3000);
 }
 bootstrap();
