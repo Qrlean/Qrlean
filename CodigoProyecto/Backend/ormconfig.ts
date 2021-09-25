cbb3cbac54c279acf2c6e32c191b11d638c05ed2
Camilo Garcia <cgarcia369@misena.edu.co>
2021-09-24 20:51:16 -0500

    chore: se cambia archivo de configuracion de typeorm para trabajar con .env y se hacer el archivo .env.example

---
+++ CodigoProyecto/Backend/ormconfig.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+export default {
+    type: <any>(process.env.DATABASE_DRIVER as unknown) || 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    synchronize: false,
+    entities: ['src/**/*.entity.ts'],
+    migrationsTableName: 'migrations',
+    migrations: ['src/database/migrations/*.ts'],
+    ssl:
+        process.env.NODE_ENV === 'production'
+            ? { rejectUnauthorized: false }
+            : false,
+    cli: {
+        migrationsDir: 'src/database/migrations',
+    },
+    seeds: ['src/database/seed/*.ts'],
+};
