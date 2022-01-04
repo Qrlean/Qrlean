ca03ab01f1c8334cf7956a5d8c664ce14faa8e18
Camilo Garcia <cgarcia369@misena.edu.co>
2022-01-04 17:45:33 -0500

    chore: se agregan templates de avisos de clases

--- CodigoProyecto/Backend/src/correo/enum/templates.enum.ts
+++ CodigoProyecto/Backend/src/correo/enum/templates.enum.ts
@@ -2,4 +2,6 @@ export enum Templates {
     newUser = 'd-41c74bab3cf141f5a95c9ea7c6f12d28',
     newPassword = 'd-d104841065e446bdb7d14b19d76626a3',
     userEdit = 'd-7a10bc64290a4295b75cf5cfc6fa58b1',
+    fiveMinutesLess = 'd-29450689ffa1473b8f358c5bea4b7410',
+    newClass = 'd-44c0e48c2ba448e49309dfdfd4ef86f9',
 }
