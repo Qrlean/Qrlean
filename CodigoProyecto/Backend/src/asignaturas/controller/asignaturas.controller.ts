12ea951aa3c626b3bb2007bd4fb09ea284552e22
Camilo Garcia <cgarcia369@misena.edu.co>
2022-01-04 17:45:03 -0500

    chore: se remueve unused import

--- CodigoProyecto/Backend/src/asignaturas/controller/asignaturas.controller.ts
+++ CodigoProyecto/Backend/src/asignaturas/controller/asignaturas.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { AsignaturasService } from '../services/asignaturas.service';
+
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { Roles } from '../../auth/decorators/roles.decorator';
 import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../../auth/guards/roles-auth.guard';
 import { Role } from '../../auth/roles/roles.enum';
-import { AsignaturasService } from '../services/asignaturas.service';
 
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard, RolesGuard)
