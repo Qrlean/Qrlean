1c8e5bad83308fb86fbaf0e1ce8d34292ff4e1a2
Camilo Garcia <cgarcia369@misena.edu.co>
2022-01-13 11:53:28 -0500

    chore: se crea seeder de los tipos de asistencias

---
+++ CodigoProyecto/Backend/src/database/seed/8tipoasistencia.seed.ts
@@ -0,0 +1,20 @@
+import { Factory, Seeder } from 'typeorm-seeding';
+import { Connection } from 'typeorm';
+import { TipoAsistencia } from '../../asistencias/entities/tipo_asistencias.entity';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bcrypt = require('bcrypt');
+export default class TipoAsistenciaSeeder implements Seeder {
+    public async run(factory: Factory, connection: Connection): Promise<any> {
+        await connection
+            .createQueryBuilder()
+            .insert()
+            .into(TipoAsistencia)
+            .values([
+                { nombre_tipo_asistencia: 'Inasistencia' },
+                { nombre_tipo_asistencia: 'Asistencia' },
+                { nombre_tipo_asistencia: 'Asistencia con retardo' },
+                { nombre_tipo_asistencia: 'Inasistencia con excusa' },
+            ])
+            .execute();
+    }
+}
