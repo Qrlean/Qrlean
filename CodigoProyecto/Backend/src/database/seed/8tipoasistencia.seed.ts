4c0b283819e2bdd8ae937a52cf34e5ba875c8e6a
Camilo Garcia <cgarcia369@misena.edu.co>
2022-01-18 16:50:30 -0500

    chore: se agrega un nuevo tipo de asistencias

--- CodigoProyecto/Backend/src/database/seed/8tipoasistencia.seed.ts
+++ CodigoProyecto/Backend/src/database/seed/8tipoasistencia.seed.ts
@@ -2,7 +2,6 @@ import { Factory, Seeder } from 'typeorm-seeding';
 import { Connection } from 'typeorm';
 import { TipoAsistencia } from '../../asistencias/entities/tipo_asistencias.entity';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const bcrypt = require('bcrypt');
 export default class TipoAsistenciaSeeder implements Seeder {
     public async run(factory: Factory, connection: Connection): Promise<any> {
         await connection
@@ -10,6 +9,7 @@ export default class TipoAsistenciaSeeder implements Seeder {
             .insert()
             .into(TipoAsistencia)
             .values([
+                { nombre_tipo_asistencia: 'Por firmar' },
                 { nombre_tipo_asistencia: 'Inasistencia' },
                 { nombre_tipo_asistencia: 'Asistencia' },
                 { nombre_tipo_asistencia: 'Asistencia con retardo' },
